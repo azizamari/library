2ce37f39519668b02df9ea057c58383e4d942ba1
azizamari <azizamari2003@gmail.com>
2021-08-05 15:39:39 +0100

    enable json and consume routers on server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const sequelize= require('./db/index')
-
+const routers= require('./routers/index')
 const app=express()
 
+
+app.use(express.json())
+app.use('/',routers)
 
 const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
