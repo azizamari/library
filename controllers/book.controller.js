f252dae3d3622693f507fb25c2003943c70637c8
azizamari <azizamari2003@gmail.com>
2021-08-06 12:29:36 +0100

    include author info when returning book

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,24 +1,37 @@
-const {book}=require('../db').models
+const {book, author}=require('../db').models
 
 exports.getAllBooks=async (req,res)=>{
     try {
-        res.status(200).json(await book.findAll());
+        res.status(200).json(await book.findAll({
+            include: {
+              model: author,
+              attributes: ['name', 'birthYear','gender'],
+            },
+          }));
     }catch(err){
         res.status(500).json('internal error')
     }
 }
 exports.getBookById=async (req,res)=>{
-    bookObj=await book.findByPk(req.params.isbn)
+    bookObj=await book.findByPk(req.params.isbn,{
+        include: {
+          model: author,
+          attributes: ['name', 'birthYear','gender'],  // return only these columns
+        },
+      })
     if(bookObj!=null)
         res.status(200).json(bookObj)
     res.status(404).json({error:`Book with isbn ${req.params.isbn} does not exist`})  
 }
-exports.postBook=(req,res)=>{
+exports.postBook=async (req,res)=>{
+    if(await author.findByPk(req.body.authorId)==null)
+        res.status(404).json({message:`Author with id ${req.body.authorId} does not exist`})
+
     book.create(req.body).then(response=>{
         res.status(201).json(response)
     }).catch(err=>{
         res.status(400).json({message:err.errors[0].message})
-    })
+    }).catch(err=>res.status(400).json({message:err}))
 }
 exports.deleteBookById=async (req,res)=>{
     bookObj=await book.findByPk(req.params.isbn)
