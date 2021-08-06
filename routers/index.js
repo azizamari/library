99943716b6bffe487170a8f2d56cfc880849f72e
azizamari <azizamari2003@gmail.com>
2021-08-06 11:46:09 +0100

    add book router to index

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const authorRouter=require('./author.router');
+const bookRouter=require('./book.router');
 const router = express.Router()
 
 router.use('/authors',authorRouter)
+router.use('/books',bookRouter)
 module.exports = router
