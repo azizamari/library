cbf664853130aaa894b928506dd0c2d2e4b63205
azizamari <azizamari2003@gmail.com>
2021-08-06 11:32:26 +0100

    fix type in book router

--- routers/author.router.js
+++ routers/author.router.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const authorRouter=express.Router();
-const authorController=require('../controllers/author.controller');
+const authorController=require('../controllers/book.controller');
 
 authorRouter.get('/:id',authorController.getAuthorById)
 authorRouter.delete('/:id',authorController.deleteAuthorById)
