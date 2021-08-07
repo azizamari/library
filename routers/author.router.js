0582e1bb405f1568043f6c139eccc69484c86216
azizamari <azizamari2003@gmail.com>
2021-08-07 14:05:56 +0100

    Add swagger author schema

--- routers/author.router.js
+++ routers/author.router.js
@@ -2,6 +2,35 @@ const express=require('express');
 const authorRouter=express.Router();
 const authorController=require('../controllers/author.controller');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Author:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The author identifier
+ *         name:
+ *           type: string
+ *           description: The author name
+ *         birthYear:
+ *           type: integer
+ *           description: The year the book author was born
+ *         gender:
+ *           type: integer
+ *           description: The author's gender ['male', 'female', 'unspecified']
+ *       example:
+ *         id: 4
+ *         name: J.K. Rowling
+ *         birthYear: 1965 
+ *         gender: female
+ */
+
 authorRouter.get('/:id',authorController.getAuthorById)
 authorRouter.put('/:id',authorController.updateAuthor)
 authorRouter.delete('/:id',authorController.deleteAuthorById)
