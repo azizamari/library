6ad093861d46a8182ae18226d62dda9799962717
azizamari <azizamari2003@gmail.com>
2021-08-07 14:06:02 +0100

    Add swagger book schema

--- routers/book.router.js
+++ routers/book.router.js
@@ -2,6 +2,41 @@ const express=require('express');
 const bookRouter=express.Router();
 const bookController=require('../controllers/book.controller');
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - isbn
+ *         - title
+ *         - authorId
+ *       properties:
+ *         isbn:
+ *           type: integer
+ *           description: The International Standard Book Number
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         releaseYear:
+ *           type: integer
+ *           description: The year the book was published
+ *         numberOfPages:
+ *           type: integer
+ *           description: The year the book was published
+ *         authorId:
+ *           type: integer
+ *           description: Foreign key for the author id
+ *       example:
+ *         isbn: 9780747532743
+ *         title: Harry Potter and the Philosopher's Stone
+ *         releaseYear: 1997
+ *         numberOfPages: 223
+ *         authorId: 4
+ */
+
 bookRouter.get('/:isbn',bookController.getBookById)
 bookRouter.put('/:isbn',bookController.updateBook)
 bookRouter.delete('/:isbn',bookController.deleteBookById)
